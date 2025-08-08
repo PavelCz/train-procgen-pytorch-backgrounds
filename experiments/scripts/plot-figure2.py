0c6e7fca12e006376c43ee9331151adf4170bb0c
Pavel C <pavelc.git@posteo.net>
2025-08-08 12:00:32 -0700

    Extract closures for better readability

--- experiments/scripts/plot-figure2.py
+++ experiments/scripts/plot-figure2.py
@@ -7,6 +7,35 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 
+def listdir(path):
+    return [os.path.join(path, d) for d in os.listdir(path)]
+
+
+def path_to_rand_percent(path):
+    """extract integer rand_percent from path info
+    path must end with integer rand_percent"""
+    out = path[-3:]
+    while not out.isdigit():
+        out = out[1:]
+    return int(out)
+
+
+def seed_collect_freq(seed: int, van_df: pd.DataFrame) -> float:
+    """returns frequency at which agent collects the inv coin
+    in the vanilla environment, for env seed"""
+    idx = van_df['seed'] == seed
+    return float(np.mean(van_df.loc[idx]["inv_coin_collected"]))
+
+
+def get_good_seed_df(df: pd.DataFrame, good_seeds: np.ndarray) -> pd.DataFrame:
+    """
+    given a dataframe with column 'seed', return new dataframe that is a subset
+    of the old one, and includes only rows with good seeds.
+    """
+    good_seed_idx = [seed in good_seeds for seed in df['seed']]
+    return df.loc[good_seed_idx]
+
+
 def main(args):
     sns.set()
 
@@ -17,34 +46,9 @@ def main(args):
     van_df = pd.read_csv(os.path.join(vanilla_coinrun_resdir, "metrics.csv"))
     max_collect_freq = 0.1
 
-    def listdir(path):
-        return [os.path.join(path, d) for d in os.listdir(path)]
-
-    def path_to_rand_percent(path):
-        """extract integer rand_percent from path info
-        path must end with integer rand_percent"""
-        out = path[-3:]
-        while not out.isdigit():
-            out = out[1:]
-        return int(out)
-
-    def seed_collect_freq(seed):
-        """returns frequency at which agent collects the inv coin
-        in the vanilla environment, for env seed"""
-        idx = van_df['seed'] == seed
-        return np.mean(van_df.loc[idx]["inv_coin_collected"])
-
-    collect_freqs = list(map(seed_collect_freq, range(np.max(van_df['seed']))))
+    collect_freqs = list(map(lambda s: seed_collect_freq(s, van_df), range(np.max(van_df['seed']))))
     collect_freqs = np.array(collect_freqs)
     (good_seeds,) = np.nonzero(collect_freqs < max_collect_freq)
-
-    def get_good_seed_df(df):
-        """
-        given a dataframe with column 'seed', return new dataframe that is a subset
-        of the old one, and includes only rows with good seeds.
-        """
-        good_seed_idx = [seed in good_seeds for seed in df['seed']]
-        return df.loc[good_seed_idx]
 
     test_rp100_resdir = os.path.join(results_dir, args.test_rp100_resdir)
     rand_percents = [path_to_rand_percent(file) for file in os.listdir(results_dir) if file.startswith("test_rand")]
@@ -54,7 +58,7 @@ def main(args):
     # sweep over training rand_percent
     csv_files = {path_to_rand_percent(path): os.path.join(path, "metrics.csv") for path in listdir(test_rp100_resdir)}
     dataframes = {k: pd.read_csv(v, on_bad_lines="skip") for k, v in csv_files.items()}
-    dataframes = {k: get_good_seed_df(df) for k, df in dataframes.items()}
+    dataframes = {k: get_good_seed_df(df, good_seeds) for k, df in dataframes.items()}
     reach_end_freqs = {k: np.mean(df["inv_coin_collected"]) for k, df in dataframes.items()}
 
     data = list(reach_end_freqs.items())
@@ -65,14 +69,14 @@ def main(args):
     # measure how often model dies or times out, ie not gets coin
     csv_files = {path_to_rand_percent(path): os.path.join(path, "metrics.csv") for path in joint_rp_paths}
     dataframes = {k: pd.read_csv(v) for k, v in csv_files.items()}
-    dataframes = {k: get_good_seed_df(df) for k, df in dataframes.items()}
+    dataframes = {k: get_good_seed_df(df, good_seeds) for k, df in dataframes.items()}
     fail_to_get_coin_freq = {k: 1 - np.mean(df["coin_collected"]) for k, df in dataframes.items()}
 
     joint_data = list(fail_to_get_coin_freq.items())
     joint_data.sort()
     joint_data = np.array(joint_data)
 
-    baseline_vanilla_df = get_good_seed_df(van_df)
+    baseline_vanilla_df = get_good_seed_df(van_df, good_seeds)
     prob_of_reaching_end_without_inv_coin = np.mean(baseline_vanilla_df["coin_collected"] == 1)
 
     figpath = "./"
